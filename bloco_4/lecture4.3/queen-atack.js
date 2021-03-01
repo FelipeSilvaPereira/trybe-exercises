c80bc3bf0c721b3dbbbdad8fc67b77b793af7ad3
Felipe Pereira <felipepereirabasenildo@gmail.com>
2021-03-01 11:27:32 -0300

    todas as possibilidades queen atack.

--- bloco_4/lecture4.3/queen-atack.js
+++ bloco_4/lecture4.3/queen-atack.js
@@ -2,8 +2,8 @@ let posicaoRainhaLinha = 4;
 let posicaoRainhaColuna = 4;
 
 //posicao peça alvo
-let posicaoPecaLinha = 7;
-let posicaoPecaColuna = 6;
+let posicaoPecaLinha = 1;
+let posicaoPecaColuna = 7;
 
 
 //identifica se o ataque foi bem sucedido
@@ -19,6 +19,45 @@ for (let supDir = 1; supDir < 8; supDir ++) {
     let colunaRainha = posicaoRainhaColuna + supDir;
 
     if (linhaRainha > 8 || colunaRainha > 8) {
+        break;
+    }
+
+    if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+  }
+}
+
+for (let supEsq = 1; supEsq < 8; supDir ++) {
+    let linhaRainha = posicaoRainhaLinha + supEsq;
+    let colunaRainha = posicaoRainhaColuna - supEsq;
+
+    if (linhaRainha > 8 || colunaRainha < 8) {
+        break;
+    }
+
+    if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+  }
+}
+
+for (let infeEsq = 1; infeEsq < 8; infeEsq ++) {
+    let linhaRainha = posicaoRainhaLinha - infeEsq;
+    let colunaRainha = posicaoRainhaColuna - infeEsq;
+
+    if (linhaRainha < 1 || colunaRainha < 1) {
+        break;
+    }
+
+    if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+  }
+}
+
+for (let infDir = 1; infDir < 8; infDir ++) {
+    let linhaRainha = posicaoRainhaLinha - infDir;
+    let colunaRainha = posicaoRainhaColuna + infDir;
+
+    if (linhaRainha < 1 || colunaRainha > 8) {
         break;
     }
 
